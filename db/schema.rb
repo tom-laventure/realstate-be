a9c06058d0771acd94e9932eac4ae45e04851065
Tom <tom.laventure42@gmail.com>
2024-09-10 21:23:19 -0700

    serializer added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_034802) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_11_035909) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,7 +21,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_034802) do
     t.datetime "updated_at", null: false
     t.string "header"
     t.string "link"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
