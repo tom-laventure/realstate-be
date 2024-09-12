6dd4971cfbe5750abece57d7c6eae0c0eb7686ff
Tom <tom.laventure42@gmail.com>
2024-09-11 20:12:45 -0700

    small bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
     protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[header link])
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[header link])
     end
   end
