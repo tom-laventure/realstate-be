0eb28cab157cdfdd211143e043a8c0949649b550
Tom <tom.laventure42@gmail.com>
2024-11-09 23:55:27 -0800

    message controller update

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -3,21 +3,26 @@ class V1::MessagesController < ApplicationController
   before_action :auth_user
   before_action :set_group
 
-  def create
-        message = @group.messages.build(message_params)
-        message.user = @current_user
+    def create
+      message = @group.messages.build(message_params)
+      message.user = @current_user
 
-        if message.save!
+      if message.save!
           # Broadcast the message to the chat channel
-          ChatChannel.broadcast_to(@group, {
-            message: message
-          })
-          render json: { status: 200, message: 'Message successfully created', data: message }, status: :ok
-        else
-          render json: { status: 422, message: 'Error creating message', errors: message.errors.full_messages }, status: :unprocessable_entity
-        end
+        ChatChannel.broadcast_to(@group, {
+          message: message
+        })
+        render json: { status: 200, message: 'Message successfully created', data: message }, status: :ok
+      else
+        render json: { status: 422, message: 'Error creating message', errors: message.errors.full_messages }, status: :unprocessable_entity
+      end
     end
     
+    def index
+      messages = @group.messages
+      render json: messages, status: :ok
+    end
+
     private
     def set_group
       @group = @current_user.groups.without_deleted.find(params['group_id'])
