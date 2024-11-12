65903a3a3b69ef8589dc6d7460fd13595f243334
Tom <tom.laventure42@gmail.com>
2024-11-11 21:14:47 -0800

    message serializer update

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,11 +1,7 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :message, :created_at, :message_owner, :message_owner_id
+  attributes :message, :created_at, :message_owner, :user_id
 
   def message_owner
     object.user.name
-  end
-
-  def message_owner_id
-    object.user.id
   end
 end
