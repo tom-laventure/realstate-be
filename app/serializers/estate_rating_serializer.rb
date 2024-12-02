094d96800180826d527ed53d5937987ab8ecea96
Tom <tom.laventure42@gmail.com>
2024-12-01 19:44:01 -0800

    minor rating update

--- app/serializers/estate_rating_serializer.rb
+++ app/serializers/estate_rating_serializer.rb
@@ -1,8 +1,12 @@
 class EstateRatingSerializer < ActiveModel::Serializer
-  attributes :rating, :id, :rating_owner
+  attributes :rating, :id, :rating_owner, :rating_owner_id
 
   def rating_owner
    object.user.name
+  end
+
+  def rating_owner_id
+    object.user.id
   end
 
 
