1e33f923b3bce7b1936ae8b1483e29aeeb39b391
Tom <tom.laventure42@gmail.com>
2025-01-04 20:11:53 -0800

    update to sub comments

--- app/serializers/estate_comment_serializer.rb
+++ app/serializers/estate_comment_serializer.rb
@@ -1,5 +1,5 @@
 class EstateCommentSerializer < ActiveModel::Serializer
-  attributes :comment, :id, :comment_owner, :comment_type, :is_author
+  attributes :comment, :id, :comment_owner, :comment_type, :is_author, :subcomment_count
 
   def comment_owner
     object.user.name
@@ -9,6 +9,10 @@ class EstateCommentSerializer < ActiveModel::Serializer
     object.user === current_user
   end
 
+  def subcomment_count
+    object.subcomments.count
+  end
+  
   private
 
   def current_user
